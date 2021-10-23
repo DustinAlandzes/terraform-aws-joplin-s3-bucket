801f0b26354b9b4aa42990d1f18f48e678a1f13f
Dustin Alandzes <5882512+DustinAlandzes@users.noreply.github.com>
2021-10-22 22:52:45 -0500

    import existing infrastructure (#1)

---
+++ main.tf
@@ -0,0 +1,84 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "dustinalandzes"
+
+    workspaces {
+      name = "personal-terraform"
+    }
+  }
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+}
+
+resource "digitalocean_droplet" "gibson_droplet" {
+  name              = "seafile-gibson"
+  region            = "sfo2"
+  tags              = ["seafile"]
+  count             = "1"
+  size              = "s-2vcpu-4gb"
+  image             = "56427524"
+  backups           = true
+  graceful_shutdown = false
+}
+
+resource "digitalocean_firewall" "gibson_firewall" {
+  name  = "gibson-firewall"
+  tags  = ["seafile"]
+  count = "1"
+  inbound_rule {
+    port_range = "22"
+    protocol   = "tcp"
+    source_addresses = [
+      "0.0.0.0/0",
+      "::/0",
+    ]
+  }
+  inbound_rule {
+    port_range = "443"
+    protocol   = "tcp"
+    source_addresses = [
+      "0.0.0.0/0",
+      "::/0",
+    ]
+  }
+  inbound_rule {
+    port_range = "80"
+    protocol   = "tcp"
+    source_addresses = [
+      "0.0.0.0/0",
+      "::/0",
+    ]
+  }
+
+  outbound_rule {
+    destination_addresses = [
+      "0.0.0.0/0",
+      "::/0",
+    ]
+    protocol = "icmp"
+  }
+  outbound_rule {
+    destination_addresses = [
+      "0.0.0.0/0",
+      "::/0",
+    ]
+    port_range = "all"
+    protocol   = "tcp"
+  }
+  outbound_rule {
+    destination_addresses = [
+      "0.0.0.0/0",
+      "::/0",
+    ]
+    port_range = "all"
+    protocol   = "udp"
+  }
+}
